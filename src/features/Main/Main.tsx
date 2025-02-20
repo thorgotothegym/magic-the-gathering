b4d98eaba20f64a308e71c738b9e55f69a471ccc
Aleix Vidal <al3ix.vidal@gmail.com>
2025-02-20 18:41:42 +0100

    feat(Main): update Main component to render cards using the Card component

--- src/features/Main/Main.tsx
+++ src/features/Main/Main.tsx
@@ -1,11 +1,19 @@
+import { Card } from '@/components/Card/Card';
 import { useGetAllCards } from '@/hooks/useGetAllCards';
 import { FC } from 'react';
 
 const Main: FC = () => {
-  const { data, isLoading, isError, error } = useGetAllCards();
+  const { cards, isLoading, isError, error } = useGetAllCards();
+
   if (isLoading) return <>{isLoading}</>;
   if (isError) return <>{error?.message}</>;
 
-  return <>{JSON.stringify(data)}</>;
+  return (
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      {cards?.map(({ name, imageUrl, type, id }) => (
+        <Card key={id} name={name} imageUrl={imageUrl} type={type} />
+      ))}
+    </div>
+  );
 };
 export default Main;
