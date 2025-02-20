3fe6515387b4aea139363b9b43100d2883589a3d
Aleix Vidal <al3ix.vidal@gmail.com>
2025-02-20 16:58:53 +0100

    feat: add Card type

---
+++ src/type/card.ts
@@ -0,0 +1,24 @@
+export type Cards = {
+  cards: Card[];
+};
+
+export enum Type {
+  Creature = 'Creature',
+  Enchantment = 'Enchantment',
+  Instant = 'Instant',
+  Sorcery = 'Sorcery',
+}
+
+export type Card = {
+  name: string;
+  manaCost: string;
+  cmc: number;
+  type: string;
+  imageUrl?: string;
+  types: Type[];
+  subtypes?: string[];
+  text: string;
+  artist: string;
+  number: string;
+  power?: string;
+};
