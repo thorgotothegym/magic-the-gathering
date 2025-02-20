97617e26a9d8588e79e739b86b3bfc3aff56796e
Aleix Vidal <al3ix.vidal@gmail.com>
2025-02-20 15:25:44 +0100

    feat(NotFound): add NotFound component for 404 error page

---
+++ src/components/NotFound/NotFound.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const NotFound: FC = () => {
+  return (
+    <section>
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the page you are looking for could not be found.</p>
+    </section>
+  );
+};
+export default NotFound;
