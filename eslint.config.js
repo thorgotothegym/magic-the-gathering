4fc7efd572763c469829540cc8a39905f2b1e590
Aleix Vidal <al3ix.vidal@gmail.com>
2025-02-20 11:13:38 +0100

    Refactor ESLint configuration to remove React plugin, update file patterns, and add TypeScript ESLint plugins; update tsconfig to specify source directory

--- eslint.config.js
+++ eslint.config.js
@@ -1,46 +1,28 @@
 import js from "@eslint/js";
 import globals from "globals";
-import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist", "node_modules"] },
+  { ignores: ["dist"] },
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      react.configs.recommended,
-    ],
-    files: ["src/**/*.{ts,tsx}"],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-      parserOptions: {
-        project: "./tsconfig.json",
-      },
     },
     plugins: {
-      react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
-      ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      "@typescript-eslint/no-unused-vars": ["error"],
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-props-no-spreading": "off",
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
-    },
-    settings: {
-      react: {
-        version: "detect",
-      },
     },
   }
 );
