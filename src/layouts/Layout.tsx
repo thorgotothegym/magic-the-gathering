f65984d02dba06c416bb7c63abbb9a2fff8b7c80
Aleix Vidal <al3ix.vidal@gmail.com>
2025-02-20 14:04:46 +0100

    feat(Layout): add Layout

---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+
+type Layout = {
+  children: ReactNode;
+};
+
+export const Layout = ({ children }: Layout) => {
+  return (
+    <div className="relative flex min-h-svh flex-col bg-current text-foreground">
+      <header>Header</header>
+      <main className="flex-1 p-6 bg-gray-100">{children}</main>
+    </div>
+  );
+};
