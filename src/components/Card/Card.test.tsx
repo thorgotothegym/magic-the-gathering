1c3965055ad8aebe7e6666dfe684e183795ce6f1
Aleix Vidal <al3ix.vidal@gmail.com>
2025-02-21 14:05:14 +0100

    feat(Card): Update Test

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -1,12 +1,14 @@
 import { render, screen } from '@testing-library/react';
-import { CardProps } from './type';
+import { CardCustomProps } from './type';
 import { expect, it } from 'vitest';
 import { Card } from './Card';
 
-const card: CardProps = {
+const card: CardCustomProps = {
   name: 'Ancestor Chosen',
   imageUrl: 'https://picsum.photos/223/310',
   type: 'Creature — Human Cleric',
+  id: '1',
+  onClick: () => {},
 };
 
 it('should render the name', () => {
@@ -24,7 +26,9 @@ it('should render the provided image', () => {
 });
 
 it('should render the default image when no imageUrl is provided', () => {
-  render(<Card name={card.name} type={card.type} />);
+  render(
+    <Card name={card.name} type={card.type} id={card.id} onClick={() => {}} />
+  );
   const img = screen.getByRole('img');
   expect(img).toHaveAttribute('src', 'https://picsum.photos/223/310');
 });
