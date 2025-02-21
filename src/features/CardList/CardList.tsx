21edad21d10421f59587de600e768096796c8c6c
Aleix Vidal <al3ix.vidal@gmail.com>
2025-02-21 13:56:21 +0100

    feat(CardList): Add cards to Collection

--- src/features/CardList/CardList.tsx
+++ src/features/CardList/CardList.tsx
@@ -1,12 +1,45 @@
-import { Card } from '@/components/Card/Card';
-import { useGetAllCards } from '@/hooks/useGetAllCards';
 import { FC } from 'react';
 
+import { Card } from '@/components/Card/Card';
+import { useGetAllCards } from '@/hooks/useGetAllCards';
+
 import styles from './CardList.module.css';
+import { CardProps } from '@/components/Card/type';
+import { addCardToCollection } from '@/hooks/useCollection';
+import useLocalStorage from '@/hooks/useLocalStorage';
+import { Collection as CollectionProps } from '@/features/Collections/type.ts';
 import Collection from '../Collections/Collection';
 
 const CardList: FC = () => {
   const { cards, isLoading, isError, error } = useGetAllCards();
+
+  const [collections, setCollections] = useLocalStorage<CollectionProps[]>(
+    'collections',
+    []
+  );
+
+  const handleAddCardToCollection = (card: CardProps) => {
+    const collectionName = prompt(
+      'Enter the collection name to add this card to:'
+    );
+    if (!collectionName) return;
+
+    // Retrieve all the information about the collection name
+    const nameCollection = collections.find(
+      (col) => col.name === collectionName
+    );
+    if (!nameCollection) {
+      alert('Collection not found!');
+      return;
+    }
+
+    const updatedCollections = addCardToCollection(
+      collections,
+      nameCollection.id,
+      card
+    );
+    setCollections(updatedCollections);
+  };
 
   // TODO : Create alert message
   if (isLoading) return <>{isLoading}</>;
@@ -17,7 +50,15 @@ const CardList: FC = () => {
       <Collection />
       <div className={styles.card}>
         {cards?.map(({ name, imageUrl, type, id }) => (
-          <Card key={id} name={name} imageUrl={imageUrl} type={type} />
+          <Card
+            onClick={() =>
+              handleAddCardToCollection({ id, name, imageUrl, type })
+            }
+            id={id}
+            name={name}
+            imageUrl={imageUrl}
+            type={type}
+          />
         ))}
       </div>
     </section>
