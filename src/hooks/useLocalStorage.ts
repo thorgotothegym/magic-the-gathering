080895978c8f169726ea367fcd24875bcf1507f7
Aleix Vidal <al3ix.vidal@gmail.com>
2025-02-21 10:28:44 +0100

    feat(hooks): Create useLocalStorage to manage data

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+
+// TODO: type initialValue
+export const useLocalStorage = <T>(key: string, initialValue: T) => {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      alert(`Error reading:' - ${error}`);
+      return initialValue;
+    }
+  });
+
+  useEffect(() => {
+    try {
+      localStorage.setItem(key, JSON.stringify(storedValue));
+    } catch (error) {
+      alert(`Error saving':' - ${error}`);
+    }
+  }, [key, storedValue]);
+
+  return { storedValue, setStoredValue };
+};
+
+export default useLocalStorage;
