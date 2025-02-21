caf5a5595860afd3c1ac2403f1fab8173462102e
Aleix Vidal <al3ix.vidal@gmail.com>
2025-02-21 13:45:09 +0100

    feat(Card): Create type and add onClick

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,12 +1,21 @@
 import { FC } from 'react';
 
 import styles from './Card.module.css';
-import { CardProps } from './type';
+import { CardCustomProps } from './type';
 
-export const Card: FC<CardProps> = ({ name, imageUrl, type }) => {
+export const Card: FC<CardCustomProps> = ({
+  name,
+  imageUrl,
+  type,
+  onClick,
+  id,
+}) => {
   const image = imageUrl ? imageUrl : 'https://picsum.photos/223/310';
+  const handleOnClick = () => {
+    onClick({ name, imageUrl, type, id });
+  };
   return (
-    <article className={styles.card}>
+    <article className={styles.card} onClick={handleOnClick}>
       <h3>{name}</h3>
       <img src={image} alt={name} />
       <p>{type}</p>
